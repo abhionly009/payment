0562849d683da1796dce5e987974c41d11d15350
abhi <50333572+abhionly009@users.noreply.github.com>
2026-06-06 10:40:13 +0530

    add code use manual offset commit

--- src/main/java/com/agh/Payment/kafka/PaymentListener.java
+++ src/main/java/com/agh/Payment/kafka/PaymentListener.java
@@ -11,6 +11,7 @@ import org.apache.kafka.clients.consumer.internals.Acknowledgements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
@@ -33,47 +34,58 @@ public class PaymentListener {
             dltTopicSuffix = "-dlt"
     )
     @KafkaListener(topics = "order-created", groupId = "payment-group-v2")
-    public void handleOrderCreated(GenericRecord event) {
+    public void handleOrderCreated(GenericRecord event, Acknowledgment ack) {
 
         String eventId =  event.get("eventId").toString();
         System.err.println("Received order event with event Id: " + event.get("eventId"));
         System.err.println("Received order event with order Id: " + event.get("orderId"));
 
-       boolean alreadyProcessed = processedEventRepository.existsById(eventId);
-
-       if (alreadyProcessed){
-           System.out.println("This event has been already processed we are ignoring it " );
-        return;
-       }
+        try {
 
 
-       String type =  event.get("type").toString();
-       double amount = (double) event.get("price");
-       String orderId =  event.get("orderId").toString();
-       String userId =  event.get("userId").toString();
-       String currency =  event.get("type").toString();
+            boolean alreadyProcessed = processedEventRepository.existsById(eventId);
 
-       PaymentRequestDTO requestDTO = new PaymentRequestDTO();
-       requestDTO.setOrderId(Long.parseLong(orderId));
-       requestDTO.setType(type);
-       requestDTO.setUserId(Long.parseLong(userId));
-       requestDTO.setAmount(amount);
-       requestDTO.setCurrency(currency);
-       paymentService.makePayment(requestDTO);
+            if (alreadyProcessed) {
+                System.out.println("This event has been already processed we are ignoring it ");
+                return;
+            }
 
-        /**
-         * This code is here to simulate crash so that when consumer
-         * restart it receive event from kafka broker and try to process payment again
-         * since processed event was not successful payment will deduct once again, Retry-able
-         * topic will retry for 3 attempts if not succeed a new topic will be created by name
-         * order-created-dlt, which will be letter inspect by concerned team re attempted and if not
-         * success then audit it.
-         */
-//        System.exit(1);
 
-       processedEventRepository.save(new PaymentEvent(eventId));
-//       System.exit(1);
+            String type = event.get("type").toString();
+            double amount = (double) event.get("price");
+            String orderId = event.get("orderId").toString();
+            String userId = event.get("userId").toString();
+            String currency = event.get("type").toString();
 
+            PaymentRequestDTO requestDTO = new PaymentRequestDTO();
+            requestDTO.setOrderId(Long.parseLong(orderId));
+            requestDTO.setType(type);
+            requestDTO.setUserId(Long.parseLong(userId));
+            requestDTO.setAmount(amount);
+            requestDTO.setCurrency(currency);
+            paymentService.makePayment(requestDTO);
+
+            /**
+             * This code is here to simulate crash so that when consumer
+             * restart it receive event from kafka broker and try to process payment again
+             * since processed event was not successful payment will deduct once again, Retry-able
+             * topic will retry for 3 attempts if not succeed a new topic will be created by name
+             * order-created-dlt, which will be letter inspect by concerned team re attempted and if not
+             * success then audit it.
+             */
+//           System.exit(1);
+
+            processedEventRepository.save(new PaymentEvent(eventId));
+
+            /**
+             * Manual offset commit here until unless all the steps are not completed
+             * offset will not be committed.
+             */
+            ack.acknowledge();
+
+        } catch (Exception ex){
+            System.err.println("Processing failed " + ex.getMessage());
+        }
         /**
          * Here we are exiting from system due to this, kafka will not be able to commit
          * offset so whenever this application will restart kafka will deliver old message
